506cf6d81a0d12f2249fdb0d715ff51d0501e9f4
ZeBitzz <9180270@esmad.ipp.pt>
2019-03-15 15:31:28 +0000

    exercicio 2

---
+++ ficha3.2.js
@@ -0,0 +1,3 @@
+document.getElementById("p1").innerHTML= "Está um dia de chuva!";
+document.querySelector("img").src="https://www.altominho.tv/site/wp-content/uploads/2019/02/chuva.jpg"
+document.getElementById("p2").style.backgroundColor ="yellow"
